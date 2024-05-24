1343f379cf1d1e0498accae30872e3eb72440c3b
Tesssssssssy <ewoo97@naver.com>
2024-05-24 22:55:57 +0900

    [Chore] 결제 기능 구현 시 필요한 기타 코드 수정
    - JwtUtils
    - ErrorCode

--- src/main/java/com/example/campingontop/exception/ErrorCode.java
+++ src/main/java/com/example/campingontop/exception/ErrorCode.java
@@ -47,6 +47,8 @@ public enum ErrorCode {
 
     // Orders 관련 에러
     ORDERS_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 주문 내역이 존재하지 않습니다."),
+    NOT_MATCH_AMOUNT(HttpStatus.BAD_REQUEST, "가격이 일치하지 않습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러"),
     ;
 
     public HttpStatus getStatus() {
