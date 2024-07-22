a091606d36da21e4b3210b570ad2c98ff30e0a2d
Tesssssssssy <ewoo97@naver.com>
2024-07-22 16:15:06 +0900

    [Chore] NOSQL - MongoDB Atlas 설정 및 백엔드 연동 (#18)

--- src/main/java/com/example/campingontop/CampingOnTopApplication.java
+++ src/main/java/com/example/campingontop/CampingOnTopApplication.java
@@ -4,13 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.example.campingontop.domain.mysql")  // MySQL repositories
+@EnableMongoRepositories(basePackages = "com.example.campingontop.domain.mongodb.chat")  // MongoDB repositories
 public class CampingOnTopApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CampingOnTopApplication.class, args);
     }
