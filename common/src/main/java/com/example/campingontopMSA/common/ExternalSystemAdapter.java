91faf76143d79555b528dfcfdce920a0d6da4c7e
Tesssssssssy <ewoo97@naver.com>
2024-08-18 05:50:23 +0900

    [Refactor] MSA 전환 - common ExternalSystemAdapter annotation 추가

---
+++ common/src/main/java/com/example/campingontopMSA/common/ExternalSystemAdapter.java
@@ -0,0 +1,15 @@
+package com.example.campingontopMSA.common;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface ExternalSystemAdapter {
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}
